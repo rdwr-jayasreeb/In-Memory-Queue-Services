3fe69251ba250cd87139576ae333b2eaf142b60c
Jayasree Banda <jayasreeb@radware.com>
2026-09-15 11:16:26 +0530

    feat : Added service Interface

---
+++ internal/service/interface.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"context"
+
+	"in-memory-queue/internal/model"
+)
+
+type QueueService interface {
+	CreateQueue(ctx context.Context, name string, maxDepth ...int) (*model.Queue, error)
+	GetQueue(ctx context.Context, name string) (*model.Queue, error)
+	ListQueues(ctx context.Context) ([]*model.Queue, error)
+	DeleteQueue(ctx context.Context, name string) error
+	Enqueue(ctx context.Context, name, body string, attributes map[string]string) (*model.Message, error)
+	Dequeue(ctx context.Context, name string) (*model.Message, error)
+	Peek(ctx context.Context, name string) (*model.Message, error)
+}
